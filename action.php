c4a95870f170dba3bd8ea741505b2c6b92960291
Sigrid <56256323+Sikiteeb@users.noreply.github.com>
2020-11-21 17:25:21 +0200

    Add files via upload

---
+++ action.php
@@ -0,0 +1,20 @@
+<?php
+//validate dropdown menu 'Choose a dog to pet' options
+
+
+$("#option.value").change(function)(){
+  var = value; 
+       switch (value) {
+           case = "Labradoodle":
+            window.location.href='AssignmentP/Labradoodle.html';
+           case ="Chow Chow":
+            window.location.href='AssignmentP/ChowChow.html';
+           case = "Keeshond":
+              window.location.href='AssignmentP/Keeshond.html';
+           case = "Nope":
+              window.location.href='AssignmentP/Cats.html';
+           break;
+       }
+       });
+
+    
