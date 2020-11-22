2993aa69a9de7af1bb0690e5ac8ea036ea56314a
Sigrid <56256323+Sikiteeb@users.noreply.github.com>
2020-11-22 18:29:58 +0200

    Update hey.php

--- AssignmentP/hey.php
+++ AssignmentP/hey.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-
-<html lang="en" dir="ltr">
-
-<body>
-
-<h2> Hey there, <?php echo $_POST['fname']. ''. $_POST['lname']; ?> </h2>
+<?php
 
 
-</body>
-</html>
+$data = $_POST;
+
+if (empty($data['fname']) ||
+    empty($data['lname']) ||
+    {
+    
+  echo "Please fill the form correctly!";
+        else{
+           echo "Thanks for registering for petting  .$fname .$lname !";
+        }
+}
+
+
+?>
