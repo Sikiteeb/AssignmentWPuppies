ffcaec81a28d93258316188a059e046906749c11
Sigrid <56256323+Sikiteeb@users.noreply.github.com>
2021-02-16 13:44:50 +0200

    Answered Theory Question2 by Sigrid

---
+++ Java11/ics0014-test1/src/main/java/ee/taltech/a_theory/question2/Functions.java
@@ -0,0 +1,15 @@
+package ee.taltech.a_theory.question2;
+
+public class Functions {
+
+    // todo check all that are TRUE [x] (letters A-H are for grading)
+    //  A [] function is a piece of code that takes in variables and returns an output | Specification: (except when it is void)
+    //  B [x] in java functions are different from methods as functions can be created separately while methods are connected to an object
+    //  C [x] method names are usually written as verbs
+    //  D [x] static methods means that the method returns no value
+    //  E [x] static void methods can be called without creating a new instance of a class
+    //  F [x] in real life static methods are used as utility functions
+    //  G [] if you want your method to be used by other classes in other packages you declare it as protected
+    //  H [x] private method are accessible only inside a class
+}
+
